dc48a903d3930a153b317609c8693c4153a966b2
Zack Tillotson <ztillots@gmail.com>
2012-07-24 15:01:39 -0700

    Refreshing analytics code

--- javascript.js
+++ javascript.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
 	googletracking();
-	addfeed('github', 'https://github.com/knick-knack.atom');
+	addfeed('github', 'https://github.com/Zack-Tillotson.atom');
 	addfeed('blog', 'http://www.excitedmuch.com/blog/feed/');
 });
 
 function googletracking() {
 	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-22535544-1']);
+	_gaq.push(['_setAccount', 'UA-33622205-1']);
 	_gaq.push(['_trackPageview']);
 
 	(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+		var ga = document.createElement('script'); 
+		ga.type = 'text/javascript'; 
+		ga.async = true;
+		ga.src = 'http://www.google-analytics.com/ga.js';
+
+		var s = document.getElementsByTagName('script')[0]; 
+		s.parentNode.insertBefore(ga, s);
+
 	})();
 }
 
@@ -20,7 +26,7 @@ function addfeed($e, $url) {
 
 	var $d = $e + "-feed";
 	
-	$("h1").after('<div class="feed" id="' + $d + '"></div>');
+	$("h1").after('<div class="feed" id="' + $d + '"><img src="loading.gif" /></div>');
 	$("#" + $d).hide();
 
 	// Show feed div
