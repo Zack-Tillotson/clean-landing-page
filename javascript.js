ce5dfbd056854fa3a87d95a0c8f815e134be0b86
Zack Tillotson <ztillots@gmail.com>
2012-08-10 14:42:24 -0700

    Adjusting style to fit mobile views too

--- javascript.js
+++ javascript.js
@@ -1,33 +1,14 @@
 $(document).ready(function() {
-	googletracking();
 	addfeed('github', 'https://github.com/Zack-Tillotson.atom');
 	addfeed('blog', 'http://www.excitedmuch.com/blog/feed/');
 	addfeed('china', 'http://www.laowaigonewild.com/feed/');
 });
 
-function googletracking() {
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-33622205-1']);
-	_gaq.push(['_trackPageview']);
-
-	(function() {
-
-		var ga = document.createElement('script'); 
-		ga.type = 'text/javascript'; 
-		ga.async = true;
-		ga.src = 'http://www.google-analytics.com/ga.js';
-
-		var s = document.getElementsByTagName('script')[0]; 
-		s.parentNode.insertBefore(ga, s);
-
-	})();
-}
-
 function addfeed($e, $url) {
 
 	var $d = $e + "-feed";
 	
-	$("h1").after('<div class="feed" id="' + $d + '"><img src="loading.gif" /></div>');
+	$("h1").after('<div class="feed" id="' + $d + '"><img class="loading" src="loading.gif" /></div>');
 	$("#" + $d).hide();
 
 	// Show feed div
